f8978dcecec21da6602b4d090a71416219abbb36
Eric-Fernandes-529 <61845397+Eric-Fernandes-529@users.noreply.github.com>
2020-04-18 15:03:20 -0400

    MySQL Functionality
    SQL module and add basic user function

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
+var mysql = require('mysql');
 
 // Initialize slackbot
 const bot = new SlackBot({
-token: 'xoxb-1055511604102-1074899502657-DQevF53q7jPdw3YMZeoUgc3B',
+token: 'xoxb-1055511604102-1064351790406-ge5ku3crxpwcACmioyDFEF3f',
 name: 'RewardBot'
 });
 
-
+var con = mysql.createConnection({
+    host: "localhost", 
+    user: "RewardBot", 
+    password: "password", 
+    database: "userscores"
+  });
 // Create start handler
 bot.on('start',() =>{
-    const params = {
-        icon_emoji: ':bar_chart:'
-    }
-
-    bot.postMessageToChannel('general', 'Hello!', params);
+    addUser("TestUser",42);
 });
 
 
 // Error handler
 bot.on('error', (err) => console.log(err));
+
+function addUser(User, Score){
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("INSERT INTO scores (user_id,score) values ('"+User+"','"+Score+"')", function (err, result,) {
+          if (err) throw err;
+          console.log(result);
+        });
+      });
+}
