aa80d67d9880ee9ad0b0303fabb36266c5e30e46
Eric-Fernandes-529 <61845397+Eric-Fernandes-529@users.noreply.github.com>
2020-04-28 23:33:24 -0400

    Bot message responses and cleanup

--- index.js
+++ index.js
@@ -4,43 +4,27 @@ var mysql = require('mysql');
 
 // Initialize slackbot
 const bot = new SlackBot({
-token: 'xoxb-1055511604102-1072423454231-W37mANVpfbUrMiEACvsy4Noj',
+token: 'xoxb-1055511604102-1072423454231-wJ1uPQx7Rb5h9DFQfq2m5RbI',
 name: 'RewardBot'
 });
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "RewardBot",
-    password: "password",
-    database: "userscores"
-  });
-
 // Create start handler
 bot.on('start',() =>{
-// addUser("TestUser",42);
-
   bot.postMessageToChannel('general', 'Hello! I\'m here to help you record your accomplishments and see where you stack up!');
-  bot.postMessageToChannel('general', 'Say: \"Show me the leaderboard\" to see the leaderboard for the day. '+
-  '\nSay: \"I finished a problem\" to let me add to your point value. \n'+
-  'Say: \"I started a problem\" to let me add to your point value. \nMake sure to @ me!');
+  bot.postMessageToChannel('general', 'Say: \"Show me the leaderboard\" to see the leaderboard for the day.\n'+
+  'Say: \"Add me to the leaderboard Handle:(TopCoder Username)\" to let me add you to the database.\n'+
+  'Say: \"I finished a problem Handle:(TopCoder Username)\" to let me add to your point value. \n'+
+  'Say: \"I started a problem Handle:(TopCoder Username)\" to let me add to your point value. \nMake sure to @ me!');
 });
 
 // Error handler
 bot.on('error', (err) => console.log(err));
-
-function addUser(User,Score){
-  con.connect(function(err) {
-    if (err) throw err;
-    con.query("INSERT INTO scores (user_id,score) values ('"+User+"','"+Score+"')", function (err, result,) {
-      if (err) throw err;
-      bot.postMessageToChannel('general', 'User "'+User+'" has been added with a score of '+Score+'.');
-    });
-  });
-}
-
 // Message Handler
 bot.on('message', data =>{
   if(data.type !== 'message'){
+    return;
+  }
+  if(data.subtype=='bot_message'){
     return;
   }
   handleMessage(data.text)
@@ -48,19 +32,23 @@ bot.on('message', data =>{
 
 // Response to data
 function handleMessage(message){
+  user="";
   if(message.includes('Handle: ')){
-    bot.postMessageToChannel('general', 'Your handle is '+message.substring(message.indexOf('Handle: ')+8));
+    user=message.substring(message.indexOf('Handle: ')+8);
+  }
+  if(message.includes('Add me to the leaderboard ')){
+    getTopCoder(user);
   }
   if(message.includes('Show me the leaderboard')){
-    bot.postLeaderboard();
+    postLeaderboard();
   }
   if(message.includes('I finished a problem')){
     bot.postMessageToChannel('general', 'Great Job! +3 Points');
-    //point addition here
+    addPoints(user,3);
   }
   if(message.includes('I started a problem')){
     bot.postMessageToChannel('general', 'Great Job! +1 Point');
-    //point addition here
+    addPoints(user,1);
   }
 }
 
@@ -69,35 +57,40 @@ function getTopCoder(handle){
   var request = require('request');
   request('http://api.topcoder.com/v2/users/'+handle, function (err, response, body) {
     len=JSON.parse(body).Achievements.length;
-    return callback(handle,len);
+    return addPoints(handle,len);
     if(err) throw err;
   });
 }
-
+// MySQL stuff
 function postLeaderboard(){
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "RewardBot",
+    password: "password",
+    database: "userscores"
+  });
   con.connect(function(err) {
     if (err) throw err;
     con.query("SELECT * FROM scores ORDER BY score DESC", function (err, result,) {
       if (err) throw err;
       bot.postMessageToChannel('general', 'Leaderboard:\n'+result);
+      con.end();
     });
   });
 }
-  // TopCoder stuff
-function getTopCoder(handle){
-  var request = require('request');
-  request('http://api.topcoder.com/v2/users/'+handle, function (err, response, body) {
-    len=JSON.parse(body).Achievements.length;
-    return addUser(handle,len);
-    if(err) throw err;
+function addPoints(User,Score){
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "RewardBot",
+    password: "password",
+    database: "userscores"
   });
-}
-function postLeaderboard(){
   con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM scores ORDER BY score DESC", function (err, result,) {
+    con.query("INSERT INTO scores(user_id,score) values('"+User+"',"+Score+") ON DUPLICATE KEY UPDATE score=score+"+Score+";", function (err, result,) {
       if (err) throw err;
-      bot.postMessageToChannel('general', 'Leaderboard:\n'+result);
+      bot.postMessageToChannel('general', 'User "'+User+'" has been added with a score of '+Score+'.');
+      con.end();
     });
   });
 }
